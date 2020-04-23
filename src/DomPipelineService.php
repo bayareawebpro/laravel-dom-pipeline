257a1dd8d66dbcec0e4c412e825af1fd902fc948
Dan Alvidrez <dan@bayareawebpro.com>
2020-04-23 03:24:29 -0700

    Update DomPipelineService.php

--- src/DomPipelineService.php
+++ src/DomPipelineService.php
@@ -7,24 +7,46 @@ use Illuminate\Pipeline\Pipeline;
 
 class DomPipelineService
 {
+    /**
+     * The pipeline instance.
+     * @var Pipeline
+     */
     protected Pipeline $pipeline;
+
+    /**
+     * The array of pipes to be processed.
+     * @var array
+     */
     protected array $pipes;
 
+    /**
+     * Make Pipeline instance & process it.
+     * @param string|null $html
+     * @param array $pipes
+     * @return mixed
+     */
     public static function make(?string $html = null, array $pipes = [])
     {
-        return app(static::class, [
-            'html'  => $html,
-            'pipes' => $pipes,
-        ])->process($html);
+        return app(static::class, compact('pipes'))->process($html);
     }
 
-    public function __construct(Pipeline $pipeline, ?string $html = null, array $pipes = [])
+    /**
+     * DomPipelineService constructor.
+     * @param Pipeline $pipeline
+     * @param array $pipes
+     */
+    public function __construct(Pipeline $pipeline, array $pipes = [])
     {
         $this->pipeline = $pipeline;
         $this->pipes = $pipes;
     }
 
-    public function process(?string $html = null)
+    /**
+     * Process Pipes
+     * @param string|null $html
+     * @return string|null
+     */
+    protected function process(?string $html = null)
     {
         if (empty($html)) return $html;
 
@@ -33,16 +55,26 @@ class DomPipelineService
             ->through($this->pipes)
             ->via('handle')
             ->then(function (DOMDocument $dom) {
-                return $this->toHtml($dom);
+                return $this->getHtml($dom);
             });
     }
 
-    protected function toHtml(DOMDocument $dom): string
+    /**
+     * Get Html Content
+     * @param DOMDocument $dom
+     * @return string
+     */
+    protected function getHtml(DOMDocument $dom): string
     {
         $html = $dom->saveHTML($dom->getElementsByTagName('body')->item(0));
         return str_replace(['<body>', '</body>'], '', $html);
     }
 
+    /**
+     * Get DOMDocument Instance with specified HTML content.
+     * @param string $html
+     * @return DOMDocument
+     */
     protected static function getDom(string $html): DOMDocument
     {
         $doc = new DOMDocument;
