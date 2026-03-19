d8f6c81af697e0e79c44544183882c96c8ee2c51
Dan Alvidrez <dan@bayareawebpro.com>
2026-03-18 22:47:23 -0700

    Update DomPipelineService.php

--- src/DomPipelineService.php
+++ src/DomPipelineService.php
@@ -8,26 +8,21 @@ use Illuminate\Support\Facades\App;
 
 class DomPipelineService
 {
-    protected Pipeline $pipeline;
-
-    protected array $pipes;
-
-    public function __construct(Pipeline $pipeline, array $pipes = [])
-    {
-        $this->pipeline = $pipeline;
-        $this->pipes = $pipes;
+    public function __construct(
+        protected Pipeline $pipeline,
+        protected array $pipes = []
+    ){
+        //
     }
-
-    public static function make(?string $html = null, array $pipes = [])
+    public static function make(string|null $html, array $pipes = [])
     {
         return App::make(static::class, compact('pipes'))->process($html);
     }
-
-    protected function process(?string $html = null): ?string
+    protected function process(string|null $html): ?string
     {
         if (empty($html)) return $html;
 
-        return (string)$this->pipeline
+        return $this->pipeline
             ->send(DomParser::make($html))
             ->through($this->pipes)
             ->via('handle')
